7c15c9a861ddd393549d6257b2fc2b1182bbbb89
ivan0527 <ivan.xing5@gmail.com>
2017-05-26 20:22:17 +0800

    ffds

---
+++ MainTest.java
@@ -0,0 +1,20 @@
+package com.itheima;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		System.out.println("初始化项目");
+		if("用户点击了搜索按钮" != null){
+			System.out.println("得到一个好友! 开始聊天!");
+			System.out.println("好友发了个红包");
+			
+			System.out.println("好友发了个表情");
+			System.out.println("admin发定位");
+			System.out.println("admin下载自定义表情");
+		}
+		if("用户摇一摇" != null){
+			System.out.println("得到一个好友! 开始聊天!");
+		}
+	}
+
+}
